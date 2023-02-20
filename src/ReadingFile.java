143c68db959f276666997a8de274c5ec7e7407fd
jorik11top <105889837+jorik11top@users.noreply.github.com>
2023-02-20 17:27:47 +0300

    Added Main.java

--- src/ReadingFile.java
+++ src/ReadingFile.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ReadingFile {
+public class ReadingFile {
+    private String fileNameAndWay;
+
+    ReadingFile(String fileNameAndWay) {
+        while (!fileSearch(fileNameAndWay)) {
+        }
+        this.fileNameAndWay = fileNameAndWay;
+    }
+
+    private boolean fileSearch(String fileNameAndWay){
+        System.out.println("tets");
+        return true;
+    }
 }
+
+
