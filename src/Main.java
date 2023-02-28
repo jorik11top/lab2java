0e21c6727722178fc4a82321bcc4ae0adf3262ef
jorik11top <105889837+jorik11top@users.noreply.github.com>
2023-03-01 02:48:18 +0300

    Added Main.java pre-release

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
 import java.io.IOException;
 import java.util.Scanner;
 //C:\tests\file1test.txt
+//C:\tests\count.txt
+//C:\tests\angel.jpg
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the path and file names:");
-        String wayAndName = in.nextLine();
-        ReadingFile file = new ReadingFile(wayAndName);
-        file.countСharAz();
-
-
+        Scanner str = new Scanner(System.in);
+        ReadingFile file = new ReadingFile();
+        while(true){
+            int userSelection;
+            System.out.println("What do you want to do?(Enter a number)");
+            System.out.println("1-Enter file for next work");
+            System.out.println("2-Count the number of letters of the English alphabet of both registers");
+            System.out.println("3-exit the program");
+            System.out.print("Your choice:");
+            userSelection = in.nextInt();
+            switch (userSelection){
+                case (1):
+                    System.out.print("Enter the path and file names:");
+                    String wayAndName = str.nextLine();
+                    file = new ReadingFile(wayAndName);
+                    break;
+                case (2):
+                    if(file.getFileNameAndWay() == null){
+                        System.out.println("You didn't specify any files");
+                        System.out.print("Enter the path and file names:");
+                        String way = str.nextLine();
+                        file = new ReadingFile(way);
+                        break;
+                    }
+                    else{
+                        file.countCharAz();
+                        break;
+                    }
+            }
+            if(userSelection == 3){
+                System.out.println("Thank you for using the program =)");
+                break;
+            }
+        }
     }
-
 }
--- src/ReadingFile.java
+++ src/ReadingFile.java
@@ -1,7 +1,7 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.nio.file.*;
+import java.util.Scanner;
+
 public class ReadingFile {
     private String fileNameAndWay;
 
@@ -20,7 +20,6 @@ public class ReadingFile {
 
     }
 
-
     public String getFileNameAndWay(){
         return fileNameAndWay;
     }
@@ -29,18 +28,47 @@ public class ReadingFile {
         return new File(fileNameAndWay).isFile();
     }
 
-    public void countСharAz()throws java.io.IOException{
+    public void countCharAz()throws java.io.IOException{
         try(FileReader file = new FileReader(this.fileNameAndWay)) {
             int symb;
+            int countHighReg = 0;
+            int countLowReg = 0;
             while ((symb = file.read()) != -1){
-                System.out.println((char) symb);
+                if(symb >= 'a' && symb <= 'z'){
+                    countLowReg++;
+                }
+                else if (symb >= 'A' && symb <= 'Z') {
+                    countHighReg++;
+                }
             }
+            System.out.println("High registr: "+countHighReg);
+            System.out.println("Low registr: "+countLowReg);
+            newFile(countHighReg,countLowReg,writeWay());
         }
-        catch(FileNotFoundException f){
+        catch(NullPointerException f){
             System.out.println("This file does not exist");
         }
 
     }
+    private void newFile(int highReg,int lowReg,String wayAndNameFile) throws IOException {
+
+        try (FileOutputStream file = new FileOutputStream(wayAndNameFile)){
+            file.write(("High registr: "+ highReg+ "\n").getBytes());
+            file.write(("Low registr: "+ lowReg).getBytes());
+            System.out.println("The data is written to a file");
+        }
+        catch(FileNotFoundException e){
+            System.out.println("This directory not found");
+        }
+    }
+    private String writeWay(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the path and file name where you want to save the data");
+        System.out.println("(You can enter the name of a new file or overwrite an existing one)");
+        System.out.print("Enter the path and file name: ");
+        return in.nextLine();
+    }
+
 }
 
 
